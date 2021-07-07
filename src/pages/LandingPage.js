b61ffc4053a22397c22ce3ac56efb866c77d8641
Danindra7 <56295369+Danindra7@users.noreply.github.com>
2021-07-07 18:08:07 +0700

    init and create button component
    -initialize react app in repo
    -create button component and test button

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "elements/Button";
+
+function LandingPage() {
+  return (
+    <div>
+      <Button> Halo</Button>
+    </div>
+  );
+}
+
+export default LandingPage;
